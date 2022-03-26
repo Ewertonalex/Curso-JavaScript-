03b1232875bd15b6e8043e98e1a41c246b90e11a
Ewerton Alexander <91163104+Ewertonalex@users.noreply.github.com>
2022-03-26 17:37:40 -0300

    Add files via upload

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade de seu carro é ${vel} KM/h`)
+if (vel > 60) {
+    console.log(`Você foi MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
